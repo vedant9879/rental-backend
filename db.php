38a129002804ceb744caf2af07cb564d2bc68b75
vedant9879 <vedantkale0999@gmail.com>
2026-03-26 15:52:59 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,15 @@
 <?php
-$conn = new mysqli("localhost","root","","rental_db");
 
-if($conn->connect_error){
- die("Connection Failed");
+$host = getenv("mysql.railway.internal");
+$user = getenv("root");
+$pass = getenv("KvBeENpOYKXzuUUKnUBTdqewBJBDoIgk");
+$db   = getenv("railway");
+$port = getenv("3306");
+
+$conn = mysqli_connect($host,$user,$pass,$db,$port);
+
+if(!$conn){
+    die("DB Connection Failed");
 }
+
 ?>
