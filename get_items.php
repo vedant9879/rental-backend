d800a0fe7fbe4a113a2f150784cffea0ef0c684d
vedant9879 <vedantkale0999@gmail.com>
2026-03-27 15:20:57 +0530

    Update get_items.php

--- get_items.php
+++ get_items.php
@@ -1,13 +1,20 @@
 <?php
 include "db.php";
 
+$base_url = "https://rental-backend-production-8cbf.up.railway.app/";
+
 $sql = "SELECT * FROM vehicles";
 $res = mysqli_query($conn,$sql);
 
 $data = array();
 
 while($row = mysqli_fetch_assoc($res)){
-    // ✅ Already full URL stored in DB → no need to change
+    
+    // ✅ Convert to full URL
+    if(!str_starts_with($row['vehicle_image'], "http")){
+        $row['vehicle_image'] = $base_url . $row['vehicle_image'];
+    }
+
     $data[] = $row;
 }
 
