ede11d1d451205d04b0f41c98a495c7154f9a643
vedant9879 <vedantkale0999@gmail.com>
2026-03-27 15:17:27 +0530

    Update get_items.php

--- get_items.php
+++ get_items.php
@@ -1,15 +1,13 @@
 <?php
 include "db.php";
 
-$base_url = "https://your-domain/"; // change
-
 $sql = "SELECT * FROM vehicles";
 $res = mysqli_query($conn,$sql);
 
 $data = array();
 
 while($row = mysqli_fetch_assoc($res)){
-    $row['vehicle_image'] = $base_url . $row['vehicle_image'];
+    // ✅ Already full URL stored in DB → no need to change
     $data[] = $row;
 }
 
