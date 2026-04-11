c9a31d897678109d66ebc47f64028e1e3d0ece1a
vedant9879 <vedantkale0999@gmail.com>
2026-04-11 22:59:06 +0530

    Update booking.php

--- booking.php
+++ booking.php
@@ -7,7 +7,6 @@ $start = $_POST['start_date'];
 $end = $_POST['end_date'];
 $total = $_POST['total_price'];
 
-// ❗ Check if already booked
 $check = $conn->query("SELECT * FROM bookings 
 WHERE vehicle_id='$vehicle'
 AND (
@@ -20,7 +19,6 @@ if($check->num_rows > 0){
     echo "already booked";
 }else{
 
-    // 🔥 ADD STATUS HERE
     $status = "pending";
 
     $conn->query("INSERT INTO bookings(user_phone,vehicle_id,start_date,end_date,total_price,status)
