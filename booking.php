3b31625ec674a52724e942c9c55212449fe6c300
vedant9879 <vedantkale0999@gmail.com>
2026-04-16 20:56:51 +0530

    Update booking.php

--- booking.php
+++ booking.php
@@ -7,6 +7,9 @@ $start = $_POST['start_date'];
 $end = $_POST['end_date'];
 $total = $_POST['total_price'];
 
+$payment = "COD"; // 🔥 CASH ON DELIVERY
+
+// Check booking
 $check = $conn->query("SELECT * FROM bookings 
 WHERE vehicle_id='$vehicle'
 AND (
@@ -19,10 +22,8 @@ if($check->num_rows > 0){
     echo "already booked";
 }else{
 
-    $status = "pending";
-
-    $conn->query("INSERT INTO bookings(user_phone,vehicle_id,start_date,end_date,total_price,status)
-    VALUES('$user','$vehicle','$start','$end','$total','$status')");
+    $conn->query("INSERT INTO bookings(user_phone,vehicle_id,start_date,end_date,total_price,payment_mode)
+    VALUES('$user','$vehicle','$start','$end','$total','$payment')");
 
     echo "success";
 }
