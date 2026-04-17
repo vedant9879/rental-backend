e3114695765422a2269fd6629edaf175ee3d36f7
vedant9879 <vedantkale0999@gmail.com>
2026-04-17 22:54:44 +0530

    Update get_my_vehicles.php

--- get_my_vehicles.php
+++ get_my_vehicles.php
@@ -2,16 +2,34 @@
 include "db.php";
 
 header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
 
 $owner = $_GET['owner_phone'] ?? '';
 
-$res = mysqli_query($conn,
-    "SELECT * FROM vehicles WHERE owner_phone='$owner' ORDER BY id DESC"
-);
+$sql = "SELECT * FROM vehicles 
+        WHERE owner_phone='$owner'
+        ORDER BY id DESC";
 
-$data = [];
+$res = mysqli_query($conn, $sql);
+
+$data = array();
 
 while($row = mysqli_fetch_assoc($res)){
+
+    // Full image URL fix
+    if(isset($row['vehicle_image']) && strpos($row['vehicle_image'], "http") !== 0){
+        $row['vehicle_image'] =
+        "https://rental-backend-production-8cbf.up.railway.app/" . $row['vehicle_image'];
+    }
+
+    // Default values
+    $row['city'] = $row['city'] ?? "";
+    $row['address'] = $row['address'] ?? "";
+    $row['quantity'] = $row['quantity'] ?? "1";
+    $row['deposit'] = $row['deposit'] ?? "0";
+    $row['price_6hr'] = $row['price_6hr'] ?? "0";
+    $row['price_12hr'] = $row['price_12hr'] ?? "0";
+
     $data[] = $row;
 }
 
