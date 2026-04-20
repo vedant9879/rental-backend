69f8134cac8f353be7f872863336902288ddc26d
vedant9879 <vedantkale0999@gmail.com>
2026-04-20 20:51:11 +0530

    Update get_my_bookings.php

--- get_my_bookings.php
+++ get_my_bookings.php
@@ -1,29 +1,45 @@
 <?php
 include "db.php";
 
-$user = $_GET['user_phone'];
+$user =
+$_GET['user_phone'] ?? '';
 
-$sql = "SELECT 
-        b.id,
-        b.user_phone,
-        b.owner_phone,
-        v.vehicle_name,
-        b.start_date,
-        b.end_date,
-        b.total_price,
-        b.quantity,
-        b.booking_plan,
-        b.status
-        FROM bookings b
-        JOIN vehicles v ON b.vehicle_id = v.id
-        WHERE b.user_phone='$user'
-        ORDER BY b.id DESC";
+if($user == ''){
+    echo "[]";
+    exit();
+}
 
-$result = mysqli_query($conn, $sql);
+$sql =
+"SELECT
+b.id,
+v.vehicle_name,
+b.user_phone,
+b.owner_phone,
+b.start_date,
+b.end_date,
+b.total_price,
+b.status,
+b.quantity,
+b.booking_plan,
+b.payment_mode
+
+FROM bookings b
+
+JOIN vehicles v
+ON b.vehicle_id = v.id
+
+WHERE b.user_phone='$user'
+
+ORDER BY b.id DESC";
+
+$result =
+mysqli_query($conn,$sql);
 
 $data = array();
 
-while($row = mysqli_fetch_assoc($result)){
+while($row =
+mysqli_fetch_assoc($result)){
+
     $data[] = $row;
 }
 
