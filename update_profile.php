e28335f6ef28ddfc49ba4cc952e37d3ae725bc02
vedant9879 <vedantkale0999@gmail.com>
2026-04-21 14:31:18 +0530

    Update update_profile.php

--- update_profile.php
+++ update_profile.php
@@ -1,24 +1,32 @@
 <?php
 include "db.php";
 
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
-$address = $_POST['address'];
-$city = $_POST['city'];
-$pincode = $_POST['pincode'];
+$name     = mysqli_real_escape_string($conn, $_POST['name']);
+$phone    = mysqli_real_escape_string($conn, $_POST['phone']);
+$email    = mysqli_real_escape_string($conn, $_POST['email']);
+$address  = mysqli_real_escape_string($conn, $_POST['address']);
+$city     = mysqli_real_escape_string($conn, $_POST['city']);
+$pincode  = mysqli_real_escape_string($conn, $_POST['pincode']);
+$aadhar   = mysqli_real_escape_string($conn, $_POST['aadhar']);
+$license  = mysqli_real_escape_string($conn, $_POST['license']);
 
-$sql = "UPDATE users SET 
+$sql = "UPDATE users SET
         name='$name',
         email='$email',
         address='$address',
         city='$city',
-        pincode='$pincode'
+        pincode='$pincode',
+        aadhar_number='$aadhar',
+        license_number='$license'
         WHERE phone='$phone'";
 
 if(mysqli_query($conn, $sql)){
-    echo json_encode(["status"=>"success"]);
+    echo json_encode([
+        "status"=>"success"
+    ]);
 }else{
-    echo json_encode(["status"=>"error"]);
+    echo json_encode([
+        "status"=>"error"
+    ]);
 }
 ?>
